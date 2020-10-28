6e75a1f17cfaa68f9845ae32c9a7324dc9d10452
DiogoSequeira <Diogo.Sequeira@ctw.bmwgroup.com>
2020-10-28 10:47:56 +0000

    added MockCommanderRepo

--- Data/MockCommanderRepo.cs
+++ Data/MockCommanderRepo.cs
@@ -1,8 +1,5 @@
 ﻿using Commander.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Commander.Data
 {
@@ -10,12 +7,19 @@ namespace Commander.Data
     {
         public IEnumerable<Command> GetAppCommands()
         {
-            throw new NotImplementedException(); asdsad
+            var commands = new List<Command>
+            {
+                new Command {Id = 0, HowTo = "Boil an Egg", Line = "Boil Water", Platform = "Pan" },
+                new Command {Id = 1, HowTo = "Boil a Potato", Line = "Boil Water", Platform = "Pan"},
+                new Command {Id = 2, HowTo = "Boil a Crab", Line = "Boil Water", Platform = "Pan" }
+            };
+
+            return commands;
         }
 
         public Command GetCommandById(int id)
         {
-            throw new NotImplementedException();
+            return new Command {Id = 0, HowTo = "Boil an Egg", Line = "Boil Water", Platform = "Pan"};
         }
     }
 }
