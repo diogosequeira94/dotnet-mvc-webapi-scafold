d6ac601366161cde514f3a4d05d8aa820e62df07
DiogoSequeira <Diogo.Sequeira@ctw.bmwgroup.com>
2020-10-28 09:54:03 +0000

    added project structure

--- Data/MockCommanderRepo.cs
+++ Data/MockCommanderRepo.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using Commander.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Commander.Data
 {
-    public class Class
+    public class MockCommanderRepo : ICommanderRepo
     {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            throw new NotImplementedException(); asdsad
+        }
+
+        public Command GetCommandById(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
